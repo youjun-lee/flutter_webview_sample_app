fa4b8e400cdbcb0d09ce79c69676a5569f8b34f5
youjun-lee <youjun.lee@toss.im>
2024-04-30 21:12:26 +0900

    home_screen style fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,  // AppBar의 배경색을 파란색으로 설정
+        foregroundColor: Colors.white,  // AppBar의 전방 색상(텍스트)을 흰색으로 설정
         title: Text('Toss payments Webview Sample'),
       ),
       body: Center(
@@ -23,25 +25,43 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                // webview_flutter 화면으로 이동
-                print('webview_flutter Launch'); // 추후 기능 추가 예정
-              },
-              child: Text('webview_flutter Launch'),
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(double.infinity, 50),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // webview_flutter 화면으로 이동
+                  print('webview_flutter Launch'); // 추후 기능 추가 예정
+                },
+                child: Text('webview_flutter Launch'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    // 모서리를 사각형으로 설정
+                    borderRadius: BorderRadius.zero,
+                  ), // 버튼 텍스트 색상을 흰색으로 설정
+                ),
               ),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // inappwebview 화면으로 이동
-                print('inappwebview Launch'); // 추후 기능 추가 예정
-              },
-              child: Text('inappwebview Launch'),
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(double.infinity, 50),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // inappwebview 화면으로 이동
+                  print('inappwebview Launch'); // 추후 기능 추가 예정
+                },
+                child: Text('inappwebview Launch'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    // 모서리를 사각형으로 설정
+                    borderRadius: BorderRadius.zero,
+                  ), // 버튼 텍스트 색상을 흰색으로 설정
+                ),
               ),
             ),
           ],
