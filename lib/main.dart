daaf74f4b971f69dd697adeb81d3dba78e04054d
youjun-lee <youjun.lee@toss.im>
2024-04-30 20:44:28 +0900

    main.dart > home_screen.dart로 홈화면 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,57 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Toss payments Webview Sample'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  initialValue: 'http://52.78.198.128/sample/', // 입력 필드의 기본값 설정
-                  decoration: InputDecoration(
-                    labelText: 'URL',
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter a URL',
-                  ),
-                ),
-              ),
-              SizedBox(height: 50), // 입력 필드와 버튼 사이의 간격
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // 여기에 webview_flutter 런칭 코드를 추가하세요.
-                    print('Launching webview_flutter');
-                  },
-                  child: Text('webview_flutter Launch'),
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(double.infinity, 50), // 버튼의 최소 크기 설정
-                  ),
-                ),
-              ),
-              SizedBox(height: 20), // 첫 번째 버튼과 두 번째 버튼 사이의 간격
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // 여기에 inappwebview 런칭 코드를 추가하세요.
-                    print('Launching inappwebview');
-                  },
-                  child: Text('inappwebview Launch'),
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(double.infinity, 50), // 버튼의 최소 크기 설정
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                initialValue: 'http://52.78.198.128/sample/', // 입력 필드의 기본값 설정
+                decoration: InputDecoration(
+                  labelText: 'URL',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a URL',
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                // webview_flutter 화면으로 이동
+                print('webview_flutter Launch'); // 추후 기능 추가 예정
+              },
+              child: Text('webview_flutter Launch'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // inappwebview 화면으로 이동
+                print('inappwebview Launch'); // 추후 기능 추가 예정
+              },
+              child: Text('inappwebview Launch'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
